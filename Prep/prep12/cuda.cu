12d6bf3e85a81f66d17fdc58c0e1d348d11f3cc1
Dexerity <ozd.martin@gmail.com>
2024-05-16 14:33:33 +0200

    Prep12 edit

--- Prep/prep12/cuda.cu
+++ Prep/prep12/cuda.cu
@@ -15,12 +15,43 @@ __global__ void kernel_mult(CudaPic inPic, CudaPic outPic)
     int half_x = inPic.m_size.x / 2;
     int half_y = inPic.m_size.y / 2;
 
-    uchar3 l_color = inPic.getData<uchar3>(x, y);
+    uchar4 l_color = inPic.getData<uchar4>(x, y);
 
-    outPic.setData<uchar3>(x / 2, y / 2, l_color);
-    outPic.setData<uchar3>(x / 2 + half_x, y / 2, l_color);
-    outPic.setData<uchar3>(x / 2, y / 2 + half_y, l_color);
-    outPic.setData<uchar3>(x / 2 + half_x, y / 2 + half_y, l_color);
+    outPic.setData<uchar4>(x / 2, y / 2, l_color);
+    outPic.setData<uchar4>(x / 2 + half_x, y / 2, l_color);
+    outPic.setData<uchar4>(x / 2, y / 2 + half_y, l_color);
+    outPic.setData<uchar4>(x / 2 + half_x, y / 2 + half_y, l_color);
+}
+
+__global__ void kernel_insertAt(CudaPic picBG, CudaPic picFG, CudaPic res, int xP, int yP)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if(x >= picBG.m_size.x) return;
+    if(y >= picBG.m_size.y) return;
+
+    res.setData<uchar4>(x, y, picBG.getData<uchar4>(x, y));
+
+    //insert image at x, y, even if half of it is outside
+    if(x >= xP && y >= yP && x < xP + picFG.m_size.x && y < yP + picFG.m_size.y)
+    {
+        uchar4 l_color = picFG.getData<uchar4>(x - xP, y - yP);
+        if(l_color.w == 0) return;
+        res.setData<uchar4>(x, y, l_color);
+    }
+}
+
+__global__ void kernel_upChannel(CudaPic inPic, CudaPic outPic)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if(x >= inPic.m_size.x) return;
+    if(y >= inPic.m_size.y) return;
+
+    uchar3 l_color = inPic.getData<uchar3>(x, y);
+    outPic.setData<uchar4>(x, y, make_uchar4(l_color.x, l_color.y, l_color.z, 255));
 }
 
 void cuda_mult(CudaPic inPic, CudaPic outPic)
@@ -33,6 +64,42 @@ void cuda_mult(CudaPic inPic, CudaPic outPic)
     dim3 l_threads(block_size, block_size);
 
     kernel_mult<<<l_blocks, l_threads>>>(inPic, outPic);
+
+    if((l_cerr = cudaGetLastError()) != cudaSuccess) {
+        printf("CUDA error [%d]: %s\n", __LINE__, cudaGetErrorString(l_cerr));
+    }
+
+    cudaDeviceSynchronize();
+}
+
+void cuda_insertAt(CudaPic picBG, CudaPic picFG, CudaPic res, int xP, int yP)
+{
+    cudaError_t l_cerr;
+
+    int block_size = 32;
+
+    dim3 l_blocks((picBG.m_size.x + block_size - 1) / block_size, (picBG.m_size.y + block_size - 1) / block_size);
+    dim3 l_threads(block_size, block_size);
+
+    kernel_insertAt<<<l_blocks, l_threads>>>(picBG, picFG, res, xP, yP);
+
+    if((l_cerr = cudaGetLastError()) != cudaSuccess) {
+        printf("CUDA error [%d]: %s\n", __LINE__, cudaGetErrorString(l_cerr));
+    }
+
+    cudaDeviceSynchronize();
+}
+
+void cuda_upChannel(CudaPic inPic, CudaPic outPic)
+{
+    cudaError_t l_cerr;
+
+    int block_size = 32;
+
+    dim3 l_blocks((inPic.m_size.x + block_size - 1) / block_size, (inPic.m_size.y + block_size - 1) / block_size);
+    dim3 l_threads(block_size, block_size);
+
+    kernel_upChannel<<<l_blocks, l_threads>>>(inPic, outPic);
 
     if((l_cerr = cudaGetLastError()) != cudaSuccess) {
         printf("CUDA error [%d]: %s\n", __LINE__, cudaGetErrorString(l_cerr));
--- Prep/prep12/main.cpp
+++ Prep/prep12/main.cpp
@@ -2,6 +2,7 @@
 #include <cuda_device_runtime_api.h>
 #include <cuda_runtime.h>
 #include <opencv2/opencv.hpp>
+#include <string>
 
 #include "cuda_img.h"
 #include "uni_mem_allocator.h"
@@ -9,22 +10,55 @@
 using namespace cv;
 
 void cuda_mult(CudaPic inPic, CudaPic outPic);
+void cuda_insertAt(CudaPic picBG, CudaPic picFG, CudaPic res, int xP, int yP);
+void cuda_upChannel(CudaPic inPic, CudaPic outPic);
 
 int main(int argc, char **argv)
 {
     UniformAllocator allocator;
     Mat::setDefaultAllocator(&allocator);
 
-    Mat inMat = imread(argv[1], IMREAD_COLOR);
-    Mat outMat = Mat(inMat.size(), CV_8UC3);
+    Mat inMat = imread(argv[1], IMREAD_UNCHANGED);
+    Mat inMat2(inMat.size(), CV_8UC4);
+    Mat outMat(inMat.size(), CV_8UC4);
 
     CudaPic inPic(inMat);
+    CudaPic inPic2(inMat2);
     CudaPic outPic(outMat);
 
-    cuda_mult(inPic, outPic);
+    cuda_upChannel(inPic, inPic2);
 
-    imwrite("result.jpg", outMat);
+    cuda_mult(inPic2, outPic);
 
+    imshow("ResultMult", outMat);
+
+    //imwrite("result.jpg", outMat);
+
+    //printf("Channels: %d\n", inMat2.channels());
+
+    int state = 0;
+    int yP = 0;
+
+    while(yP < inPic.m_size.y)
+    {
+        //turn state into string
+        std::string stateStr = std::to_string(state);
+        
+        Mat curMat = imread(stateStr + ".png", IMREAD_UNCHANGED);
+        CudaPic curPic(curMat);
+
+        cuda_insertAt(inPic2, curPic, outPic, yP, 200);
+
+        yP += 2;
+        state++;
+        if(state > 6)
+            state = 0;
+
+        //imwrite("res/" + std::to_string(yP) + ".jpg", outMat);
+        imshow("Result", outMat);
+
+        waitKey(100);
+    }
 
 
     return 0;
